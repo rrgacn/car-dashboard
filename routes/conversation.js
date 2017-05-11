16037f2f12be5aa3cfd5c7fe2f3f056f0b400acb
rrgacn <rewati.raman.gautam@accenture.com>
2017-05-11 15:09:51 +1000

    Updated config

--- routes/conversation.js
+++ routes/conversation.js
@@ -20,8 +20,8 @@ const watson = require('watson-developer-cloud'); // watson sdk
 const conversation = watson.conversation({
   // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
   // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  // username: '<username>',
-  // password: '<password>',
+   username: 'rewati.raman.gautam@accenture.com',
+   password: 'I@baba@330',
   version_date: '2016-10-21',
   version: 'v1'
 });
